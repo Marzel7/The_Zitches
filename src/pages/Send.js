19624f8d01bc8dcf52a61e41c01d67a0e90fc8ac
Kevin Keaveney <kevinkeaveney@gmail.com>
2022-01-14 14:53:16 +0000

    align send btn

--- src/pages/Send.js
+++ src/pages/Send.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { formatEther } from '@ethersproject/units'
 import { useEtherBalance, useEthers } from '@usedapp/core'
-import { Stack, Text, Box } from '@chakra-ui/react'
+import { Stack, Text, Box, Button, InputGroup, Input, InputRightElement } from '@chakra-ui/react'
 import { ethers } from "ethers";
 
 
 export default function Send() {
     const { account } = useEthers()
     const balance = useEtherBalance(account)
+    const show = true
 
     return (
         <Box w="600px"
@@ -26,13 +27,37 @@ export default function Send() {
                     <Text>Send transaction</Text>
                     <Stack  isInline fontWeight='semibold' >
                         <Text justify-content='right'>Ether balance</Text>
-                        <Text color='gray.500'>{formatEther(balance)}</Text>
+
+                        {balance && (
+                        <Text color='gray.500'>{formatEther(balance)}</Text> )}
                         <Text justify-content='right'>eth</Text>
                     </Stack>
                 </Stack>
             </Box>
-            <Box></Box>
-        </Box>
+            <Box width={600} p={1}>
+            <InputGroup size='md'>
+                <Input
+                    pr='4.5rem'
+                    type={show ? 'text' : 'password'}
+                    placeholder='Eth to:'
+                />
+                <InputRightElement width='6.5rem'>
+                    <Button h='1.75rem' w='5rem' size='sm'
+                    px={4}
+                    variant='outline'
+                    _hover={{
+                        boxShadow: 'sm',
+                        background: "gray.900",
+                        color: "gray.100",
+                    }}
+                    _active= "gray.900">
+                     Send
+                    </Button>
+                </InputRightElement>
+                </InputGroup>
+            </Box>
+            </Box>
+
     )
 }
 
