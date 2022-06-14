1a16a5b3a97ccc982fbcb1b047949311d61234b9
kkeaveney <kevinkeaveney@gmail.com>
2022-06-14 18:11:30 +0100

    liquidity tests

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -22,46 +22,46 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
       typeof value === "string" ? value : value.toString()
     );
 
-  await deploy("Balloons", {
+  await deploy("DYDX", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     // args: [ "Hello", ethers.utils.parseEther("1.5") ],
     log: true,
   });
 
-  const balloons = await ethers.getContract("Balloons", deployer);
+  const DYDX = await ethers.getContract("DYDX", deployer);
 
   await deploy("DEX", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    args: [balloons.address],
+    args: [DYDX.address],
     log: true,
     waitConfirmations: 5,
   });
 
   const dex = await ethers.getContract("DEX", deployer);
-  // paste in your front-end address here to get 10 balloons on deploy:
-  // await balloons.transfer(
+  // paste in your front-end address here to get 10 DYDX on deploy:
+  // await DYDX.transfer(
   //   "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
   //   "" + 10 * 10 ** 18
   // );
 
-  let balance = await balloons.balanceOf(dex.address);
+  let balance = await DYDX.balanceOf(dex.address);
   console.log("DEX balance", fromWei(balance));
   // // uncomment to init DEX on deploy:
   console.log(
-    "Approving DEX (" + dex.Address + ") to take Balloons from main account..."
+    "Approving DEX (" + dex.Address + ") to take DYDX from main account..."
   );
   // // If you are going to the testnet make sure your deployer account has enough ETH
-  await balloons.approve(dex.address, ethers.utils.parseEther("100"));
+  await DYDX.approve(dex.address, ethers.utils.parseEther("100"));
   console.log("INIT exchange...");
   await dex.init(ethers.utils.parseEther("5"), {
     value: ethers.utils.parseEther("5"),
     gasLimit: 200000,
   });
 
-  balance = await balloons.balanceOf(dex.address);
+  balance = await DYDX.balanceOf(dex.address);
   console.log("DEX balance", fromWei(balance));
 };
 
-module.exports.tags = ["Balloons", "DEX"];
+module.exports.tags = ["DYDX", "DEX"];
